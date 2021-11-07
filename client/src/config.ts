669e59707d0ef4a6e01b65c08a7d860618e33730
Folusho Oladipo <fdoladipo@gmail.com>
2021-11-07 19:57:53 +0100

    Restructure project code
    Add other parts from the starter project including the:
    - React-based client.
    - README.md for the starter project.
    - Postman collection for the starter project.

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = 'sc5io1n49c'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-5lq53hqm.us.auth0.com',
+  clientId: 'kKifaNCS8HoKpsLvlpFTrDHtc4VcOR97',
+  callbackUrl: 'http://localhost:3000/callback'
+}
